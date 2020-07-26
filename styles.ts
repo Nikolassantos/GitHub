a20d2c7412a31b207a8d4118fbe55464e4a7c98d
Nikolas dos Santos <58566087+Nikolassantos@users.noreply.github.com>
2020-07-25 23:46:55 -0300

    Initial commit

---
+++ styles.ts
@@ -0,0 +1,58 @@
+import React from 'react';
+import Constants from 'expo-constants';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    marginTop: Constants.statusBarHeight + 20,
+    width: '100%'
+  },
+  TextInput: {
+    backgroundColor: '#E5E5E5',
+    width: '90%',
+    height: 50,
+    borderRadius: 10,
+    marginTop: 10
+  },
+  Header: {
+    width: '100%',
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+  Title: {
+    fontSize: 20,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
+  Icon: {
+    marginLeft: 5,
+    fontSize: 30
+  },
+  Banner: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  Main: {
+    marginTop: 10,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    height: '100%'
+  },
+  repoView: {
+    flexDirection: 'row',
+    width: '90%',
+    height: '15%',
+    alignItems: 'center'
+  },
+  AvatarIMG: {
+    width: '30%',
+    height: '90%',
+    marginLeft: '3%'
+  }
+});
+
+export default styles;
